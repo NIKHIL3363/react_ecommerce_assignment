70ee8d24ff3fa0db1082492eb844de50b8494fa7
NIKHIL3363 <mutkulenikhil87@gmail.com>
2024-04-03 11:18:25 +0530

    messagee

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import store from '../store';
 
 function Home() {
+         const l=useSelector(state=>state.cart.carts).length
+
     return (
 
 
-<div className='w-full bg-black-600 h-screen border border-red-500' >
-          <div className='text-right mx-3 mt-3'>
-          <NavLink to='/Products' className='text-blue-800 '>Products</NavLink><br></br>
-          <NavLink to='/Cart' className='text-blue-800'>Cart</NavLink><br></br>
-          <NavLink to='/checkout' className='text-blue-800'>checkout</NavLink>
+<div className='w-30 h-20 bg-black-600  border border-black-500 bg-blue-800 rounded-xl' >
+          <div className=' mx-3 mt-4'>
+          <NavLink   to='/Products' className='text-black-800 mr-6'>Products</NavLink>
+          <NavLink to='/Cart' className='text-black-800 mr-6 px-2'>Cart({l||0})</NavLink>
+          <NavLink to='/checkout' className='text-blue-1000 ml-6'>checkout</NavLink>
 
 
 
